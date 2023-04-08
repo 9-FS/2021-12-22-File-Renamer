ce20698d65889a2046d7c54ea228f664741f0d71
9FS <pray4spam@googlemail.com>
2023-04-08 17:11:38 +0000

    init

---
+++ readme.md
@@ -0,0 +1,45 @@
+---
+Topic: "Filename Dateformat Changer"
+Author: "구FS"
+---
+<link href="./src/KFS/md_style.css" rel="stylesheet"></link>
+<div id="global">
+
+# <p style="text-align: center">Filename Dateformat Changer</p>
+<br>
+<br>
+
+- [1. General](#1-general)
+- [2. How to Use](#2-how-to-use)
+
+## 1. General
+
+This program is intended to change the date format of file names. It renames according to the `Filename Dateformat Changer.csv`.
+
+## 2. How to Use
+
+1. Execute the program once. This will create a default `Filename Dateformat Changer.csv`.
+1. Fill your lines out like this:
+    ```csv
+    {input datetime format}\t{input timezone offset}\t{output datetime format}\t{output timezone offset}
+    ```
+
+    Example:
+    ```csv
+    input datetime format	input timezone offset	output datetime format	output timezone offset
+    
+    # samsung camera
+    %Y%m%d_%H%M%S		%Y-%m-%d %H_%M_%S	
+    %Y%m%d_%H%M%S(%f)		%Y-%m-%d %H_%M_%S	
+    
+    # dropbox
+    %Y-%m-%d %H.%M.%S		%Y-%m-%d %H_%M_%S	
+    %Y-%m-%d %H.%M.%S(%f)		%Y-%m-%d %H_%M_%S	
+
+    # apple
+    Foto %d.%m.%y, %H %M %S	+02:00	%Y-%m-%d %H_%M_%S	+00:00
+    ```
+1. Use the datetime expressions from the python [datetime](https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior) library.
+1. Execute either `main_outer.py` with python or a `Filename Dateformat Changer.exe` directly.
+
+</div>
---
+++ src/main.py
@@ -0,0 +1,101 @@
+#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+import csv
+import datetime as dt
+import dateutil.tz  #timezones
+import inspect
+import KFS.config, KFS.fstr, KFS.log
+import logging
+import os
+import pandas
+import io
+
+
+@KFS.log.timeit
+def main() -> None:
+    CONFIG_FILENAME: str="Filename Dateformat Changer.csv"  #config filename
+    config_df: pandas.DataFrame                         #loaded configurations
+    CONFIG_HEADER: list[str]=["input datetime format", "input timezone offset", "output datetime format", "output timezone offset"]
+    filename_base: str                                  #filename without extension
+    filename_DT: dt.datetime                            #filename as DT
+    filename_extension: str                             #filename extension (with dot)
+    filenames: list[str]=[filename for filename in os.listdir(".") if os.path.isfile(filename)] #filenames in directory current
+
+    config_filecontent_default: str=("# Fill your lines out according to the csv header below.\n"    #default config content
+    "# Use the datetime expressions from \"https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior\".\n"
+    f"{chr(9).join(CONFIG_HEADER)}\n\n" #chr(9)="\t", but backslashes in fstring not allowed
+    "# samsung camera\n"
+    "%Y%m%d_%H%M%S\t\t%Y-%m-%d %H_%M_%S\t\n"
+    "%Y%m%d_%H%M%S(%f)\t\t%Y-%m-%d %H_%M_%S\t\n\n"
+    "# dropbox\n"
+    "%Y-%m-%d %H.%M.%S\t\t%Y-%m-%d %H_%M_%S\t\n"
+    "%Y-%m-%d %H.%M.%S(%f)\t\t%Y-%m-%d %H_%M_%S\t")
+
+    try:
+        config_df=pandas.read_csv(io.StringIO(KFS.config.load_config(CONFIG_FILENAME, config_filecontent_default)),
+                                  comment="#",              #ignore comments
+                                  on_bad_lines="skip",      #will do my own checking
+                                  quoting=csv.QUOTE_NONE,   #don't encapsulate data in quotes
+                                  sep="\t")                 #tab as data separator because it can't be used in filenames
+    except FileNotFoundError:
+        logging.info("Press enter to close program.")
+        input() #pause
+        return
+    logging.info(config_df)
+    if config_df["input datetime format"].isnull().values.any():    #is any  input datetime format NaN? #type:ignore
+        logging.error(f"An input datetime format is NaN. This is most likely because one of your rows does not contain exactly {len(CONFIG_HEADER)-1} tabs to make {len(CONFIG_HEADER)} columns.")
+        raise ValueError(f"Error in {main.__name__}{inspect.signature(main)}: An input datetime format is NaN. This is most likely because one of your rows does not contain exactly {len(CONFIG_HEADER)-1} tabs to make {len(CONFIG_HEADER)} columns.")
+    if config_df["output datetime format"].isnull().values.any():   #is any  output datetime format NaN? #type:ignore
+        logging.error(f"An output datetime format is NaN. This is most likely because one of your rows does not contain exactly {len(CONFIG_HEADER)-1} tabs to make {len(CONFIG_HEADER)} columns.")
+        raise ValueError(f"Error in {main.__name__}{inspect.signature(main)}: An output datetime format is NaN. This is most likely because one of your rows does not contain exactly {len(CONFIG_HEADER)-1} tabs to make {len(CONFIG_HEADER)} columns.")
+
+    config_df_TZ_in =config_df[CONFIG_HEADER[1]].isnull()
+    config_df_TZ_out=config_df[CONFIG_HEADER[3]].isnull()
+    logging.debug(config_df_TZ_in)
+    logging.debug(config_df_TZ_out)
+
+    config_df=config_df.fillna("")  #replace NaN with "" so "nan" is not inserted as timezone information
+
+    i=0
+    while i<len(config_df_TZ_in) and i<len(config_df_TZ_out):
+        if config_df_TZ_in.values[i]==False and config_df_TZ_out.values[i]==False:  #if timezones should be changed:
+            config_df.iloc[i, 0]+="%z"                                              #change input format so it takes timezone information #type:ignore
+            logging.debug(f"Changed row [{i}] {CONFIG_HEADER[0]} to {config_df.iloc[i, 0]}.")
+        i+=1
+
+    
+    for filename_old in filenames:                                      #go through all filenames and try to rename
+        logging.info("--------------------------------------------------")
+        logging.info(f"Filename old: \"{filename_old}\"")
+        filename_base     =os.path.splitext(filename_old)[0]            #filename without extension
+        filename_extension=os.path.splitext(filename_old)[1].lower()    #file extension, convert to lowercase
+        logging.debug(f"filename base: \"{filename_base}\"")
+        logging.debug(f"filename extension: \"{filename_extension}\"")
+        
+        for i, config_row in config_df.iterrows():    #try to rename with every input format
+            logging.debug(f"Converting \"{filename_base}{config_row.iloc[1]}\" to datetime with format \"{config_row.iloc[0]}\"...")
+            try:
+                filename_DT=dt.datetime.strptime(f"{filename_base}{config_row.iloc[1]}", config_row.iloc[0]) 
+                #str -> DT
+            except ValueError:                                                                          #if input format does not work: try next one
+                logging.debug(f"\rConverting \"{filename_base}{config_row.iloc[1]}\" to datetime with format \"{config_row.iloc[0]}\" failed.")
+                continue
+            logging.debug(f"\rConverted \"{filename_base}{config_row.iloc[1]}\" to datetime with format \"{config_row.iloc[0]}\".")
+            if config_df_TZ_in[int(i)]==False and config_df_TZ_out[int(i)]==False:  #if timezones should be changed: #type:ignore
+                filename_DT=filename_DT.astimezone(dateutil.tz.tzoffset(None, int(config_row.iloc[3][0:3])*3600+int(config_row.iloc[3][4:6])*60))   #change timezone                                                                  
+                logging.debug(f"Changed datetime timezone offset to \"{KFS.fstr.notation_tech(int(config_row.iloc[3][0:3])*3600+int(config_row.iloc[3][4:6])*60, 0, round_static=True)}s\".")
+            filename_new=f"{filename_DT.strftime(config_row.iloc[2])}{filename_extension}"  #DT -> str, re-add file extension
+            logging.info(f"Filename new: \"{filename_new}\"")
+            
+            logging.info(f"Renaming \"{filename_old}\" to \"{filename_new}\"...")
+            try:
+                os.rename(f"{filename_old}", f"{filename_new}")  #rename
+            except FileExistsError: #if file already exists
+                logging.error(f"Renaming \"{filename_old}\" to \"{filename_new}\" failed, because file already exists.")
+            except PermissionError: #if file still in use:
+                logging.error(f"Renaming \"{filename_old}\" to \"{filename_new}\" failed with \"PermissionError\". File might still be in use.")
+            except OSError:         #wenn Umbenennung verbotene Zeichen enthält: aufhören
+                logging.error(f"Renaming \"{filename_old}\" to \"{filename_new}\" failed with \"OSError\". Output format might contain characters forbidden for filenames. (\\/:*?\"<>|)")
+            else:
+                logging.info(f"\rRenamed \"{filename_old}\" to \"{filename_new}\".")
+            break                   #in any case: filename next
+    return
